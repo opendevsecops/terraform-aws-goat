aa5f5ef5a778507a77a45fb945ed872199757cbc
Petko D. Petkov <pdp@gnucitizen.org>
2019-01-04 22:50:04 +0000

    Added bucket with read.

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+module "s3_bucket_public_read" {
+  source = "modules/s3-bucket-public-read"
+}
+
 module "s3_bucket_public_read_write" {
   source = "modules/s3-bucket-public-read-write"
 }
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "s3_bucket_public_read_id" {
+  value = "${module.s3_bucket_public_read.id}"
+}
+
 output "s3_bucket_public_read_write_id" {
   value = "${module.s3_bucket_public_read_write.id}"
 }

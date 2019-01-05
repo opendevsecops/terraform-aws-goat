64653fc2cc8562260b1ce42a64fa2906f3cbb4a8
Petko D. Petkov <pdp@gnucitizen.org>
2019-01-05 10:20:45 +0000

    Remove default content.

--- modules/s3-bucket-public-read-write/main.tf
+++ modules/s3-bucket-public-read-write/main.tf
@@ -3,11 +3,3 @@ module "bucket" {
 
   acl = "public-read-write"
 }
-
-module "bucket_object_readme" {
-  source = "../_s3_bucket_object"
-
-  bucket = "${module.bucket.id}"
-  key    = "README.txt"
-  file   = "${path.module}/contents/README.txt"
-}
